4cffb8a521ded6b3ddbd4e0cce4c203302a450c1
Luis Alvarez <luis.alvarez@stem.com>
2017-04-27 16:27:46 -0700

    fixed syntax error

--- my_scraper/scraper_app/models.py
+++ my_scraper/scraper_app/models.py
@@ -7,17 +7,6 @@ import settings
 
 DeclarativeBase = declarative_base()
 
-def db_connect():
-    """
-    Performs database connection using database settings from settings.py.
-    Returns sqlalchemy engine instance
-    """
-    return create_engine(URL(**settings.DATABASE))
-
-def create_deals_table(engine):
-    """"""
-    DeclarativeBase.metadata.create_all(engine)
-
 class Deals(DeclarativeBase):
     """Sqlalchemy deals model"""
     __tablename__ = "deals"
@@ -28,4 +17,15 @@ class Deals(DeclarativeBase):
     location = Column('location', String, nullable=True)
     original_price = Column('original_price', String, nullable=True)
     price = Column('price', String, nullable=True)
-    end_date = Column('end_date', DateTime, nullable=True
+    end_date = Column('end_date', DateTime, nullable=True)
+
+def db_connect():
+    """
+    Performs database connection using database settings from settings.py.
+    Returns sqlalchemy engine instance
+    """
+    return create_engine(URL(**settings.DATABASE))
+
+def create_deals_table(engine):
+    """"""
+    DeclarativeBase.metadata.create_all(engine)
